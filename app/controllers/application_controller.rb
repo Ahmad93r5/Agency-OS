2eacedb702ac51ceb9a35c9c9361c9ae626ac339
Ahmad Rehman <ahmadrehman414@gmail.com>
2026-08-06 19:59:40 +0500

    Implement user module & Jwt signup, Login

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::API
+    before_action :authorize_request
+             # ApplicationController mein laga diya hai.
+             # Iska matlab Signup aur Login bhi token maangenge.
+    
+
+    skip_before_action :authorize_request, only: [:signup, :login]
+    # yaha islia {authorize_request} add kia ha kukay ya sb controller pa apply hoga 
+
+    private 
+
+    def authorize_request
+          token = request.headers["Authorization"]&.split(" ")&.last
+        #  Jab frontend ya Postman protected API call karega, wo header bhejega:
+        # Space ke basis par tod dega: --> (split)
+        # Last element de dega: ---> .last
+        # safe navigation operator --> &.
+        payload = JsonWebToken.decode(token)
+        # Yani payload variable ke andar ab token ka data aa gaya.    
+        @current_user = User.find(payload["user_id"])
+        # @current_user instance variable hai.
+
+        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+  render json: { error: "Unauthorized" }, status: :unauthorized
+    
+    end
+end
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,34 @@
+class AuthenticationController < ApplicationController
+    def signup
+      user = User.new(user_params)
+      if user.save
+        render json:{message: "User Register Successfully"}, status: :created
+      else
+        render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+
+    def login
+    user =  User.find_by(email: params[:user][:email])
+      
+      if user&.authenticate(params[:user][:password])
+        token = JsonWebToken.encode(user_id: user.id)
+                                    # user.id --> db sa araha ha 
+        
+        render json: {
+            token: token
+            # json key = ruby variable
+        },status: :ok
+      else
+          render json: {
+            message: "Invalid Email or Password"
+          }, status: :unauthorized
+        end
+    end
+    
+    private
+    def user_params
+       params.require(:user).permit(:name, :email, :password)
+    end
+ 
+end
